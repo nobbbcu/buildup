f2a5f9358ccf93feb334b40dd76bb884947feb91
nobbbcu <nobbcu@gmail.com>
2019-12-27 10:41:45 +0900

    changed basic design

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
-    @nickname = user.nickname
+    @name = user.name
     @tweets = user.tweets
   end
 end
