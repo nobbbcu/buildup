49d7dfa1e5aec4660c3b5da0582379084a7bce29
nobbbcu <nobbcu@gmail.com>
2019-12-27 23:11:00 +0900

    added user edit function

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   resources :tweets do
     resources :comments, only: :create
   end
-  resources :users, only: :show
+  resources :users, only: [:show, :edit, :update]
 end
